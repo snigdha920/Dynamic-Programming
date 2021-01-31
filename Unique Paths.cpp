0b8f34cd60ee5a74e6290456a427d51a6fc5c7db
Snigdha Singh <62167899+snigdha920@users.noreply.github.com>
2021-02-01 01:02:00 +0530

    faster runtime

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -3,18 +3,16 @@ public:
     int paths[110][110];
     int uniquePaths(int m, int n) {
         memset(paths, -1, sizeof(paths));
-        return calcUniquePaths(m-1, n-1, m, n);
+        return calcUniquePaths(0, 0, m, n);
     }
     int calcUniquePaths(int r, int c, int m, int n) {
-        if(r < 0 || c < 0) return 0;
-        if(r == 0 || c == 0) return 1;
+        if(r < 0 || c < 0 || r >= n || c >= m) return 0;
+        if(r == n-1) return 1;
+        if(c == m-1) return 1;
         if(paths[r][c] != -1) {
-            printf("path[%d][%d]: %d", r, c, paths[r][c]);
             return paths[r][c];
         }
-        
-        paths[r][c] = calcUniquePaths(r-1, c, m, n) + calcUniquePaths(r, c-1, m, n);
-        printf("path[%d][%d]: %d", r, c, paths[r][c]);
+        paths[r][c] = calcUniquePaths(r, c+1, m, n) + calcUniquePaths(r+1, c, m, n);
         return paths[r][c];
     }
 };
